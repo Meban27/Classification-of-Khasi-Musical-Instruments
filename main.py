1344147dd6227d6510fa2567ac8cd5d16aef07f3
Mebanpynshai Lyngdoh Marshilong <134036432+Meban27@users.noreply.github.com>
2024-06-25 22:14:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ import numpy as np
 import librosa
 import joblib
 import pandas as pd
+from PIL import Image
 
 # Load your trained model
 model = joblib.load('khasi.pkl')
+
 
 # Feature extraction function
 def extract_features(audio_file):
@@ -37,40 +39,92 @@ def extract_features(audio_file):
                             columns=['adsr', 'zero_crossing_rate', 'spectral_centroid', 'spectral_rolloff', 'spectral_flux'])
     return features
 
+
+logo = Image.open('logo.jpg')  # Ensure 'logo.png' is in the same directory
+desired_width = 150  # Adjust as needed
+aspect_ratio = logo.size[1] / logo.size[0]
+new_size = (desired_width, int(desired_width * aspect_ratio))
+logo_resized = logo.resize(new_size)
+
+st.sidebar.image(logo_resized, use_column_width=False)
+
+page = st.sidebar.radio("# Navigation", ["Home", "About"])
+
+if page == "Home":
 # Create the Streamlit app
-st.title("Instrument Prediction from Audio")
+    st.title("Khasi Instrument Prediction from Audio")
 
-# Upload audio file
-uploaded_file = st.file_uploader("Choose an audio file", type=["wav", "mp3", "ogg"])
+    # Upload audio file
+    uploaded_file = st.file_uploader("Choose an audio file", type=["wav", "mp3", "ogg"])
 
-if uploaded_file is not None:
-    # Extract features
-    features = extract_features(uploaded_file)
+    if uploaded_file is not None:
+        # Extract features
+        features = extract_features(uploaded_file)
+        st.audio(uploaded_file)
+        
+        # Predict the instrument
+        prediction = model.predict(features)
+
+        if(prediction==1):
+            st.write("## The instrument is a Duitara")
+            st.image("duitara.jpg",caption='Duitara')
+        elif(prediction==2):
+            st.write("## The instrument is an Singphong")
+            st.image("singphong.jpg",caption='Singphong')
+        elif(prediction==3):
+            st.write("## The instrument is a Besli ")
+            st.image("besli.jpg",caption='Besli')
+        elif(prediction==4):
+            st.write("## The instrument is a Bom")
+            st.image("Bom.jpg",caption='Bom')
+        elif(prediction==5):
+            st.write("## The instrument is a Ksing Kynthei")
+            st.image("ksing kynthei.jpg",caption='Ksing kynthei')
+        elif(prediction==6):
+            st.write("## The instrument is a Ksing Shyngrang")
+            st.image("ksing shynrang.jpg",caption='Ksing Shynrang')
+        elif(prediction==7):
+            st.write("## The instrument is a Pdiah")
+            st.image("pdiah.jpg",caption='Pdiah')
+        
+
+        
+        # Display the result
+        
+
+        # Optionally display extracted features
+        st.write("Extracted Features:")
+        st.write(features)
+elif page == "About":
+    # About page content
     
-    # Predict the instrument
-    prediction = model.predict(features)
+    st.write("""
+# Classification of Khasi Musical Instruments
+This web application serves as an educational platform, showcasing the final semester project of Group #2 from the NEHU B.Tech 2020-24 batch. The project focuses on the classification of Khasi musical instruments, developed by:
 
-    if(prediction==1):
-        st.write("The instrument is a Duitara")
-    elif(prediction==2):
-        st.write("The instrument is an Singphong")
-    elif(prediction==3):
-        st.write("The instrument is a Besli ")
-    elif(prediction==4):
-        st.write("The instrument is a Bom")
-    elif(prediction==5):
-        st.write("The instrument is a Ksing Kynthei")
-    elif(prediction==6):
-        st.write("The instrument is a Ksing Shyngrang")
-    elif(prediction==7):
-        st.write("The instrument is a Pdiah")
-    
+- Mebanpynshai Lyngdoh Marshilong (21BtechLECE07)
+- Moirangthem Bhorot Singh (20BTechECE)
+- Yongyo HM (21BtechLECE01)
 
-    
-    # Display the result
-    #st.write(f"Predicted Instrument: {prediction[0]}")
+## Project Overview
+Our primary goal is to bridge the gap between Khasi musical instruments and modern technology. This project aims to analyze and classify these traditional instruments, thereby promoting their rich cultural heritage and encouraging further academic and technological exploration.
 
-    # Optionally display extracted features
-    st.write("Extracted Features:")
-    st.write(features)
+## Features
+Audio File Upload: Users can upload an audio file of a Khasi musical instrument. The app will analyze the audio and predict the instrument type.
+Instrument Prediction: Leveraging advanced machine learning techniques, the app identifies the instrument from the uploaded audio.
+Informative Results: After prediction, the app provides information about the identified instrument, accompanied by relevant images.
 
+## Datasets
+Due to an agreement with the Department of Culture and Creative, NEHU Shillong, the datasets used in this project are not publicly available. We invite students and researchers to contribute by collecting additional datasets and proposing innovative enhancements to further this project.
+
+## Contributions
+This project is developed for educational and non-profit purposes. It should not be used for personal gain but rather to foster further development in this field. We welcome contributions such as new data, improved algorithms, or other enhancements. If you have ideas or improvements, feel free to fork the repository and submit a pull request.
+
+## Future Work
+This project lays the groundwork for the integration of Khasi musical instruments with modern technology. We hope it will inspire continued research and development. Potential future work includes:
+
+- Expanding the dataset with more diverse and comprehensive samples.
+- Enhancing the classification algorithms for better accuracy.
+- Developing interactive applications or interfaces for Khasi musical instruments.
+- We are excited to see how this project evolves and improves over time, contributing to the preservation and celebration of Khasi musical heritage.
+""")
